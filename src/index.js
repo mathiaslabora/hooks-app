10e6fc190add1fd7b37fa1659d70a8544176db39
mathiaslabora <mathiaslabora@gmail.com>
2023-05-16 16:08:56 -0300

    First commit, react course

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { HooksApp } from "./HooksApp";
+import { CounterApp } from "./01-useState/CounterApp";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <HooksApp />
+    <CounterApp/>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
